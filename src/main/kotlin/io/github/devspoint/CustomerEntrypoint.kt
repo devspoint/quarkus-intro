11da1ce11988974ccbbc71040fe8c3a7c0d61d55
Diego Lirio <diegolirio.dl@gmail.com>
2021-11-19 16:09:10 -0300

    fix-without-spring-data

--- src/main/kotlin/io/github/devspoint/CustomerEntrypoint.kt
+++ src/main/kotlin/io/github/devspoint/CustomerEntrypoint.kt
@@ -1,7 +1,5 @@
 package io.github.devspoint
 
-import io.github.devspoint.springdata.CustomerSpringDataEntity
-import io.github.devspoint.springdata.CustomerSpringDataRepository
 import javax.inject.Inject
 import javax.transaction.Transactional
 import javax.ws.rs.*
@@ -12,9 +10,6 @@ import javax.ws.rs.core.MediaType
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 class CustomerEntrypoint {
-
-    @Inject
-    private lateinit var customerSpringDataRepository: CustomerSpringDataRepository
 
     @POST
     fun created(customer: CustomerEntity): CustomerEntity {
@@ -39,17 +34,4 @@ class CustomerEntrypoint {
         CustomerEntity.deleteById(id)
     }
 
-    @PUT
-    @Path("/{id}")
-    fun update(@PathParam("id") id: Long, customer: CustomerEntity) {
-        CustomerEntity.findById(id)?.let {
-            it.copy(firstname = customer.firstname, lastname = customer.lastname)
-        }?.persist()
-    }
-
-    @GET
-    @Path("/spring")
-    fun getListSpring(@QueryParam("lastname") lastname: String): Iterable<CustomerSpringDataEntity>? {
-        return customerSpringDataRepository.findByLastname(lastname)
-    }
 }
